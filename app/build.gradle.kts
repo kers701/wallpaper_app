51a34f03726fb39a10565647a9433970b48a51f5
kers701 <1481289@qq.com>
2026-08-28 10:07:10 +0800

    发布3.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kers.killove.jhsy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 310
-        versionName = "3.1.0"
+        versionCode = 3101
+        versionName = "3.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
