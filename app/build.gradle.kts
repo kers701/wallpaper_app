d90f5931bf1e8bef72fff9f14ce05fbe032355ab
kers701 <1481289@qq.com>
2026-08-31 00:51:19 +0800

    发布3.2.1，修复构建失败

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kers.killove.jhsy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3200
-        versionName = "3.2.0"
+        versionCode = 3201
+        versionName = "3.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
