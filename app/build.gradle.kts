de62445e11d07d94d7d4da6dc936a2cd057116b1
kers701 <1481289@qq.com>
2026-08-31 06:18:51 +0800

    发布3.2.4，自定义图标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kers.killove.jhsy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3203
-        versionName = "3.2.3"
+        versionCode = 3204
+        versionName = "3.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
