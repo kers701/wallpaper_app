c8c7578de73656efb0707387ab5a6bc2fed3e15e
kers701 <1481289@qq.com>
2026-09-10 20:54:19 +0800

    发布4.2.22修复重大推送异常，并将版本号升级为4.2.22以触发软件更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kers.killove.jhsy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4103
-        versionName = "4.1.3"
+        versionCode = 4222
+        versionName = "4.2.22"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
